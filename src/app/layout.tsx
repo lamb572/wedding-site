709d299987a2c38b21b3c0b3a1e2397b5972fb53
lamb572 <lukebeach90@gmail.com>
2025-01-15 20:11:31 +0000

    Update nav bar with icons and collapse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import DisableDraftMode from "@/client/components/DisableDraftMode"
 import NavBar, { NavBarItem } from "@/client/components/NavBar"
 import ThemeWrapper from "@/client/components/ThemeWrapper"
 import { getSanityTheme } from "@/sanity/server"
+import { Box } from "@mui/material"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -9,6 +10,12 @@ import type { Metadata } from "next"
 import { VisualEditing } from "next-sanity"
 import { Birthstone } from "next/font/google"
 import { draftMode } from "next/headers"
+import HomeIcon from "@mui/icons-material/Home"
+import ScheduleIcon from "@mui/icons-material/Schedule"
+import ModeOfTravelIcon from "@mui/icons-material/ModeOfTravel"
+import RsvpIcon from "@mui/icons-material/Rsvp"
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark"
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard"
 import "./globals.css"
 
 const leagueScript = Birthstone({
@@ -41,31 +48,37 @@ export default async function RootLayout({
       key: "home",
       href: "/",
       text: "Home",
+      icon: <HomeIcon color="primary" />,
     },
     {
       key: "schedule",
       href: "/schedule",
       text: "Schedule",
+      icon: <ScheduleIcon color="primary" />,
     },
     {
       key: "travel",
       href: "/travel",
       text: "Travel & Accommodation",
+      icon: <ModeOfTravelIcon color="primary" />,
     },
     {
       key: "rsvp",
       href: "/rsvp",
       text: "RSVP",
+      icon: <RsvpIcon color="primary" />,
     },
     {
       key: "faq",
       href: "/faq",
       text: "FAQ",
+      icon: <QuestionMarkIcon color="primary" />,
     },
     {
       key: "registry",
       href: "/registry",
       text: "Registry",
+      icon: <CardGiftcardIcon color="primary" />,
     },
   ]
 
@@ -74,8 +87,18 @@ export default async function RootLayout({
       <body className={leagueScript.variable}>
         <AppRouterCacheProvider>
           <ThemeWrapper sanityTheme={sanityTheme}>
-            <NavBar open={showNav} navBarItems={navBarItems} />
-            {children}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "16px",
+                height: "100%",
+                width: "100%",
+              }}
+            >
+              {showNav && <NavBar open={showNav} navBarItems={navBarItems} />}
+              {children}
+            </Box>
             <Analytics />
             <SpeedInsights />
             {(await draftMode()).isEnabled && (
--- src/client/components/NavBar/index.tsx
+++ src/client/components/NavBar/index.tsx
@@ -1,7 +1,12 @@
 "use client"
-import { Drawer } from "@mui/material"
-import NavBarItem, { NavBarItemProps } from "../NavBarItem"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import ChevronRightIcon from "@mui/icons-material/ChevronRight"
+import { Box, IconButton, Theme } from "@mui/material"
+import { SystemStyleObject } from "@mui/system"
 import { usePathname } from "next/navigation"
+import { useRef, useState } from "react"
+import { Transition, TransitionStatus } from "react-transition-group"
+import NavBarItem, { NavBarItemProps } from "../NavBarItem"
 
 export interface NavBarItem extends Omit<NavBarItemProps, "selected"> {
   key: string
@@ -13,18 +18,74 @@ export interface NavBarProps {
 }
 
 export default function NavBar({ open, navBarItems }: NavBarProps) {
+  const [navBarOpen, setNavBarOpen] = useState(open)
   const pathName = usePathname()
 
+  const nodeRef = useRef(null)
+  const transitionStyles: Record<TransitionStatus, SystemStyleObject<Theme>> = {
+    entering: {
+      flexGrow: 1,
+      width: "fit-content",
+      maxWidth: "200px",
+    },
+    entered: {
+      flexGrow: 1,
+      width: "fit-content",
+      maxWidth: "200px",
+    },
+    exiting: {
+      width: "fit-content",
+      maxWidth: "60px",
+    },
+    exited: {
+      width: "fit-content",
+      maxWidth: "60px",
+    },
+    unmounted: {},
+  }
+
   return (
-    <Drawer open={open}>
-      {navBarItems.map(({ key, href, ...item }) => (
-        <NavBarItem
-          {...item}
-          key={key}
-          href={href}
-          selected={href === pathName}
-        />
-      ))}
-    </Drawer>
+    <Transition nodeRef={nodeRef} in={navBarOpen} timeout={300}>
+      {(state) => (
+        <Box
+          sx={(theme) => ({
+            transition: `width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms`,
+            overflow: "hidden",
+            display: "flex",
+            flexFlow: "column nowrap",
+            gap: theme.spacing(2),
+            border: "1px grey",
+            boxShadow:
+              "0px 0px 0px 0px rgba(0,0,0,0.2),3px 0px 3px 0px rgba(0,0,0,0.14)",
+            ...transitionStyles[state],
+          })}
+        >
+          <Box
+            sx={(theme) => ({
+              display: "flex",
+              alignItems: "center",
+              padding: theme.spacing(0, 1),
+              borderBottom: "rgba(0,0,0,0.2) solid",
+              ...theme.mixins.toolbar,
+              justifyContent: navBarOpen ? "flex-end" : "flex-start",
+            })}
+          >
+            <IconButton onClick={() => setNavBarOpen((c) => !c)}>
+              {navBarOpen ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </Box>
+
+          {navBarItems.map(({ key, href, ...item }) => (
+            <NavBarItem
+              {...item}
+              key={key}
+              href={href}
+              selected={href === pathName}
+              hideText={!navBarOpen}
+            />
+          ))}
+        </Box>
+      )}
+    </Transition>
   )
 }
