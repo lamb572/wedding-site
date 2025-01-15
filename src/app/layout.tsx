bb3bb5ad9ba6fa5715a503fac4efe71dc8105b25
lamb572 <lukebeach90@gmail.com>
2025-01-15 15:14:00 +0000

    enable nav bar and init routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import DisableDraftMode from "@/client/components/DisableDraftMode"
+import NavBar, { NavBarItem } from "@/client/components/NavBar"
 import ThemeWrapper from "@/client/components/ThemeWrapper"
 import { client, Theme } from "@/sanity"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
 import { VisualEditing } from "next-sanity"
 import { Birthstone } from "next/font/google"
 import { draftMode } from "next/headers"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 
 const leagueScript = Birthstone({
@@ -32,11 +33,52 @@ export default async function RootLayout({
     {}
   )
 
+  const showNav = Boolean(process.env.NEXT_MAIN_SITE_FLAG)
+
+  const navBarItems: NavBarItem[] = [
+    // {
+    //   key: "save-date",
+    //   href: "/save-date",
+    //   text: "Save Date",
+    // },
+    {
+      key: "home",
+      href: "/",
+      text: "Home",
+    },
+    {
+      key: "schedule",
+      href: "/schedule",
+      text: "Schedule",
+    },
+    {
+      key: "travel",
+      href: "/travel",
+      text: "Travel & Accommodation",
+    },
+    {
+      key: "rsvp",
+      href: "/rsvp",
+      text: "RSVP",
+    },
+    {
+      key: "faq",
+      href: "/faq",
+      text: "FAQ",
+    },
+    {
+      key: "registry",
+      href: "/registry",
+      text: "Registry",
+    },
+  ]
+
   return (
     <html lang="en">
       <body className={leagueScript.variable}>
         <AppRouterCacheProvider>
           <ThemeWrapper sanityTheme={sanityTheme}>
+            <NavBar open={showNav} navBarItems={navBarItems} />
             {children}
             <Analytics />
             <SpeedInsights />
