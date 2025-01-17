5f923b9daae403d40ebfd80f172b0df0b146b9d3
lamb572 <lukebeach90@gmail.com>
2025-01-17 10:56:23 -0500

    Update portable text component to handle color

--- src/client/components/PortableText/index.tsx
+++ src/client/components/PortableText/index.tsx
@@ -1,8 +1,9 @@
+"use client"
 import {
   portableTextStringInterpolation,
   TextBlock,
 } from "@/utils/stringInterpolation"
-import { Typography } from "@mui/material"
+import { Box, Theme, Typography, TypographyProps } from "@mui/material"
 import {
   PortableTextReactComponents,
   PortableText as SanityPortableText,
@@ -13,11 +14,42 @@ export interface PortableTextProps {
   stringInterpolationData?: Record<string, string>
 }
 
+export type TypographyColor = TypographyProps["color"]
+
+const getPaletteColor = (theme: Theme, color: TypographyColor) => {
+  if (color === "textSecondary") return theme.palette.text.secondary
+  if (color === "primary") return theme.palette.primary.main
+  if (color === "secondary") return theme.palette.secondary.main
+  if (color === "success") return theme.palette.success.main
+  if (color === "error") return theme.palette.error.main
+  if (color === "info") return theme.palette.info.main
+  if (color === "warning") return theme.palette.warning.main
+  if (color === "textPrimary") return theme.palette.text.primary
+  if (color === "textDisabled") return theme.palette.text.disabled
+}
+
 export default function PortableText({
   value,
   stringInterpolationData,
 }: PortableTextProps) {
   const components: Partial<PortableTextReactComponents> = {
+    marks: {
+      color: ({ children, value }) => {
+        const color = value.color as TypographyColor
+
+        return (
+          <Box
+            component="span"
+            sx={(theme) => {
+              const paletteColor = getPaletteColor(theme, color)
+              return { color: paletteColor }
+            }}
+          >
+            {children}
+          </Box>
+        )
+      },
+    },
     block: {
       h1: ({ children }) => <Typography variant="h1">{children}</Typography>,
 
