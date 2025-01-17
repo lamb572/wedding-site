b38e678592c61881c16b837239a7a8791c2cef2a
lamb572 <lukebeach90@gmail.com>
2025-01-16 19:34:55 -0500

    Fix width

--- src/app/save-date/page.tsx
+++ src/app/save-date/page.tsx
@@ -24,6 +24,7 @@ export default async function SaveTheDatePage() {
         alignItems: "center",
         alignContent: "center",
         justifyContent: "center",
+        width: "100%",
       }}
     >
       <SaveTheDateCard
