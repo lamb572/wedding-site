0c43602134d018f8cc79a3a65634407b79bc275a
lamb572 <lukebeach90@gmail.com>
2025-03-09 19:45:22 +0000

    add card to travel

--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,6 +1,7 @@
+import Card from "@/client/components/Card"
 import GoogleMap from "@/client/components/Map"
 import PortableText from "@/client/components/PortableText"
-import { getTravelAccommodation } from "@/sanity/server"
+import { getSettings, getTravelAccommodation } from "@/sanity/server"
 import { CookieKeys } from "@/server/cookies/types"
 import { getInviteById } from "@/server/Invite"
 import { TextBlock } from "@/utils/stringInterpolation"
@@ -8,6 +9,8 @@ import { Box, Typography } from "@mui/material"
 import { cookies } from "next/headers"
 
 export default async function TravelPage() {
+  const settings = await getSettings()
+
   const cookieStore = await cookies()
   const inviteId = cookieStore.get(CookieKeys.INVITE)?.value
 
@@ -17,83 +20,87 @@ export default async function TravelPage() {
   const userInvitedToCeremony = invite?.ceremony ?? false
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 4,
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        py: 4,
-        px: 2,
-      }}
-    >
+    <Card backgroundColor={`${settings?.card?.backgroundColor}`}>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           gap: 4,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          py: 4,
+          px: 2,
         }}
       >
-        <Typography
-          variant="h2"
-          component="h2"
-          color="primary"
-          sx={{
-            fontSize: { xs: "10vw", md: "3.75rem" },
-            textAlign: "center",
-          }}
-        >
-          {travelAccommodation?.travelHeading}
-        </Typography>
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
-            border: "1px solid black",
-            padding: 2,
-            gap: 2,
-            backgroundColor: "white",
-            borderRadius: 4,
+            gap: 4,
           }}
         >
-          <PortableText
-            value={(travelAccommodation?.travelDetails ?? []) as TextBlock}
-          />
-        </Box>
+          <Typography
+            variant="h2"
+            component="h2"
+            color="primary"
+            sx={{
+              fontSize: { xs: "10vw", md: "3.75rem" },
+              textAlign: "center",
+            }}
+          >
+            {travelAccommodation?.travelHeading}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              padding: 2,
+              gap: 2,
+              backgroundColor: "white",
+              borderRadius: 4,
+              textAlign: "initial",
+            }}
+          >
+            <PortableText
+              value={(travelAccommodation?.travelDetails ?? []) as TextBlock}
+            />
+          </Box>
 
-        <Typography
-          variant="h2"
-          component="h2"
-          color="primary"
-          sx={{
-            fontSize: { xs: "10vw", md: "3.75rem" },
-            textAlign: "center",
-          }}
-        >
-          {travelAccommodation?.accommodationHeading}
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            border: "1px solid black",
-            padding: 2,
-            gap: 2,
-            backgroundColor: "white",
-            borderRadius: 4,
-          }}
-        >
-          <PortableText
-            value={
-              (travelAccommodation?.accommodationDetails ?? []) as TextBlock
-            }
-          />
-        </Box>
+          <Typography
+            variant="h2"
+            component="h2"
+            color="primary"
+            sx={{
+              fontSize: { xs: "10vw", md: "3.75rem" },
+              textAlign: "center",
+            }}
+          >
+            {travelAccommodation?.accommodationHeading}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid black",
+              padding: 2,
+              gap: 2,
+              backgroundColor: "white",
+              borderRadius: 4,
+              textAlign: "initial",
+            }}
+          >
+            <PortableText
+              value={
+                (travelAccommodation?.accommodationDetails ?? []) as TextBlock
+              }
+            />
+          </Box>
 
-        <GoogleMap invitedToCeremony={userInvitedToCeremony} />
+          <GoogleMap invitedToCeremony={userInvitedToCeremony} />
+        </Box>
       </Box>
-    </Box>
+    </Card>
   )
 }
