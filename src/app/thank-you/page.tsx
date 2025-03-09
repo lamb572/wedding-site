4dd251fcc8d31bfe61b52901157b6e2eb84d7672
lamb572 <lukebeach90@gmail.com>
2025-03-09 19:36:08 +0000

    center thank you on page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -8,6 +8,7 @@ export default async function ThankYouPage() {
         flexDirection: "column",
         gap: 4,
         width: "100%",
+        height: "100%",
         alignItems: "center",
         justifyContent: "center",
       }}
