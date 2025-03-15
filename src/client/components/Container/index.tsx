5ad9c325b7bf5360b092df2fb18098beef5588a3
lamb572 <lukebeach90@gmail.com>
2025-03-15 20:48:31 +0000

    Refactor Container component to use function syntax and improve type definitions

--- src/client/components/Container/index.tsx
+++ src/client/components/Container/index.tsx
@@ -1,15 +1,28 @@
 import { Box } from "@mui/material"
-import { styled } from "@mui/material/styles"
-import { BoxProps } from "@mui/system"
+import { OverrideProps } from "@mui/material/OverridableComponent"
+import { BoxTypeMap } from "@mui/system"
+import { PropsWithChildren } from "react"
 
-export const Container = styled(Box)<BoxProps>(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: theme.spacing(2),
-  alignItems: "center",
-  justifyContent: "center",
-  border: "2px solid black",
-  padding: theme.spacing(2),
-  backgroundColor: "white",
-  borderRadius: theme.spacing(4),
-}))
+export interface ContainerProps
+  extends OverrideProps<BoxTypeMap, React.ElementType>,
+    PropsWithChildren {}
+
+export function Container(props: ContainerProps) {
+  return (
+    <Box
+      {...props}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        border: "2px solid black",
+        padding: 2,
+        backgroundColor: "white",
+        borderRadius: 4,
+        ...props.sx,
+      }}
+    />
+  )
+}
