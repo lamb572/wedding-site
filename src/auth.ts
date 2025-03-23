c93d001416dccdbc3a6951ab2164c3977292b935
lamb572 <lukebeach90@gmail.com>
2025-03-23 14:54:24 +0000

    Add sign-in callback to handle user authentication logic

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,17 @@ import Discord from "next-auth/providers/discord"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord],
+  callbacks: {
+    signIn({ profile }) {
+      const email = profile?.email
+      // todo: if email is in users
+      // todo: update user in db
+      // todo: allow user to sign in
+
+      // todo: if email is not in users
+      // todo: return false/error
+
+      return true
+    },
+  },
 })
