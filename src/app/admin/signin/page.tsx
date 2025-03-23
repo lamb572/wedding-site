661db45eeb15c2fa1a7237205645b1c26afd79f3
lamb572 <lukebeach90@gmail.com>
2025-03-23 17:11:10 +0000

    Implement user authentication with error handling and add getUser function

--- src/app/admin/signin/page.tsx
+++ src/app/admin/signin/page.tsx
@@ -1,11 +1,25 @@
 import Card from "@/components/Card"
 import SignIn from "@/components/SignIn"
 import { getSettings } from "@/sanity/server"
+import { Typography } from "@mui/material"
 
-export default async function SignInPage() {
+interface SignInPageProps {
+  searchParams: Promise<{
+    error?: string
+  }>
+}
+
+export default async function SignInPage({ searchParams }: SignInPageProps) {
+  const search = await searchParams
+  const error = search.error
   const settings = await getSettings()
   return (
     <Card backgroundColor={`${settings?.card?.backgroundColor}`}>
+      {error && (
+        <Typography variant="h6" color="error">
+          {error}
+        </Typography>
+      )}
       <SignIn />
     </Card>
   )
--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,21 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
+import { getUser } from "./server/users/getUsers"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord],
   callbacks: {
-    signIn({ profile }) {
+    async signIn({ profile }) {
       const email = profile?.email
-      // todo: if email is in users
-      // todo: update user in db
-      // todo: allow user to sign in
 
-      // todo: if email is not in users
-      // todo: return false/error
+      if (!email) {
+        return encodeURI("/admin/signin/?error=Email not found")
+      }
+
+      const user = await getUser({ email })
+      if (!user) {
+        return encodeURI("/admin/signin/?error=unauthorized")
+      }
 
       return true
     },
