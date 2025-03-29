601d63d6bdf75edabb9cd4c85b44f4c531145e49
lamb572 <lukebeach90@gmail.com>
2025-03-29 17:24:17 +0000

    Refactor authentication setup to use external config and prevent edge issues

--- src/auth.ts
+++ src/auth.ts
@@ -1,23 +1,23 @@
-import NextAuth from "next-auth"
-import Discord from "next-auth/providers/discord"
-import { getUser } from "./server/users/getUsers"
+import NextAuth from 'next-auth';
+import authConfig from './auth.config';
+import { getUser } from './server/users/getUsers';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  ...authConfig,
   callbacks: {
     async signIn({ profile }) {
-      const email = profile?.email
+      const email = profile?.email;
 
       if (!email) {
-        return encodeURI("/admin/signin/?error=Email not found")
+        return encodeURI('/admin/signin/?error=Email not found');
       }
 
-      const user = await getUser({ email })
+      const user = await getUser({ email });
       if (!user) {
-        return encodeURI("/admin/signin/?error=unauthorized")
+        return encodeURI('/admin/signin/?error=unauthorized');
       }
 
-      return true
+      return true;
     },
   },
-})
+});
