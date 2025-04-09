b41190711d2c5be2460f61290bc7e32598a33945
lamb572 <lukebeach90@gmail.com>
2025-04-09 21:21:08 +0100

    Enhance TableHeaderRow component: add optional sx prop for custom styling

--- src/client/components/Table/TableHeaderRow/index.tsx
+++ src/client/components/Table/TableHeaderRow/index.tsx
@@ -1,12 +1,18 @@
+import { SxProps } from '@mui/material';
 import { PropsWithChildren } from 'react';
 import TableRow from '../TableRow';
 
-export default function TableHeaderRow({ children }: PropsWithChildren) {
+interface TableHeaderRowProps extends PropsWithChildren {
+  sx?: SxProps;
+}
+
+export default function TableHeaderRow({ children, sx }: TableHeaderRowProps) {
   return (
     <TableRow
       sx={{
         border: '1px solid black',
         backgroundColor: 'primary.light',
+        ...sx,
       }}
     >
       {children}
