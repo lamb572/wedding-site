107a9efb71453f52672709c1181129d002c0ae0f
lamb572 <lukebeach90@gmail.com>
2025-04-13 20:29:14 +0100

    refactor: optimize MetricCards layout and remove unnecessary components

--- src/client/components/MetricCards/index.tsx
+++ src/client/components/MetricCards/index.tsx
@@ -1,5 +1,5 @@
 import { MetricsInvite } from '@/server/Invite/getMetricsInvite';
-import { Card, Grid2, Stack, Typography } from '@mui/material';
+import { Card, Grid2, Typography } from '@mui/material';
 
 interface MetricCardsProps {
   metrics: MetricsInvite[];
@@ -9,7 +9,7 @@ export default function MetricCards({ metrics }: MetricCardsProps) {
   return (
     <Grid2
       container
-      spacing={6}
+      spacing={2}
       direction="row"
       sx={{
         justifyContent: 'space-around',
@@ -17,30 +17,42 @@ export default function MetricCards({ metrics }: MetricCardsProps) {
       }}
     >
       {metrics.map((metric) => (
-        <Grid2 key={metric.label} size={4}>
+        <Grid2
+          key={metric.label}
+          size={{ xs: 12, sm: 6, md: 3, lg: 2 }}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Card
             sx={{
-              width: 'fit-content',
+              width: { xs: '150px' },
+              height: { xs: '150px' },
               padding: 2,
               backgroundColor: 'primary.light',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: 2,
             }}
           >
-            <Stack gap={2}>
-              <Typography
-                variant="h5"
-                component={'h2'}
-                sx={{ textWrap: 'nowrap' }}
-              >
-                {metric.label}
-              </Typography>
-              <Typography
-                variant="h5"
-                component={'h3'}
-                color="primary.contrastText"
-              >
-                {metric.value}
-              </Typography>
-            </Stack>
+            <Typography
+              variant="h5"
+              component={'h2'}
+              sx={{ textWrap: { xs: 'pretty' } }}
+            >
+              {metric.label}
+            </Typography>
+            <Typography
+              variant="h5"
+              component={'h3'}
+              color="primary.contrastText"
+            >
+              {metric.value}
+            </Typography>
           </Card>
         </Grid2>
       ))}
