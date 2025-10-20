071e03819e8e678ef96776dcc635bcd1b3a2a3ee
Luke B <lukebeach90@gmail.com>
2025-10-20 09:13:27 +0100

    Develop (#24)
    * refactor: update SignIn component to use NextAuth for authentication and standardize import quotes
    
    * feat: add CreateUserModal component for creating invites and integrate it into AdminView
    
    * disable RSVP form
    
    * refactor: standardize quotes and improve error handling in getInviteById function
    
    ---------
    
    Co-authored-by: Luke B <114909158+luke-guider@users.noreply.github.com>

--- src/server/Invite/getInviteById/index.ts
+++ src/server/Invite/getInviteById/index.ts
@@ -1,36 +1,43 @@
-"use server"
-import { stringSanitize } from "@/utils/stringSanitize"
-import { captureException } from "@sentry/nextjs"
-import { getInvite } from "../getInvite"
-import { Invite, rsvpFormSchema } from "../types"
+'use server';
+import { stringSanitize } from '@/utils/stringSanitize';
+import { captureException } from '@sentry/nextjs';
+import { getInvite } from '../getInvite';
+import { Invite, rsvpFormSchema } from '../types';
 
 export interface GetInviteByIdParams {
-  inviteId?: string
+  inviteId?: string;
 }
 
 export async function getInviteById({
   inviteId: id,
-}: GetInviteByIdParams): Promise<Omit<Invite, "_id"> | undefined> {
+}: GetInviteByIdParams): Promise<Omit<Invite, '_id'> | undefined> {
   try {
     if (!id) {
-      console.error("inviteId is not set")
-      return undefined
+      console.error('inviteId is not set');
+      return undefined;
     }
-    const inviteId = stringSanitize(id)
+    const inviteId = stringSanitize(id);
 
-    const invite = await getInvite({ inviteId })
+    if (!inviteId) {
+      const noInvite = new Error('Invite Id not sent');
+      noInvite.cause = 'Invite id missing frm params';
+      captureException(noInvite);
+      return undefined;
+    }
 
-    const result = rsvpFormSchema.safeParse(invite)
+    const invite = await getInvite({ inviteId });
+
+    const result = rsvpFormSchema.safeParse(invite);
 
     if (!result.success) {
-      captureException(result.error)
-      console.error(result.error)
-      return undefined
+      captureException(result.error);
+      console.error(result.error);
+      return undefined;
     }
 
-    return result.data
+    return result.data;
   } catch (err: unknown) {
-    captureException(err)
-    console.error(err)
+    captureException(err);
+    console.error(err);
   }
 }
